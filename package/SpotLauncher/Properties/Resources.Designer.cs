98d423b1314faa60cb1dede16b6d92bdad010f36
Elyor UzEx <elyor@outlook.com>
2024-06-12 11:28:39 +0500

    update version

--- package/SpotLauncher/Properties/Resources.Designer.cs
+++ package/SpotLauncher/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace SpotLauncher.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -61,7 +61,7 @@ namespace SpotLauncher.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 20052024.
+        ///   Looks up a localized string similar to 11062024.
         /// </summary>
         internal static string AppVersion {
             get {
